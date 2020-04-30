a119df34482776db48317ac66452790c557e9ad1
Joe Ellingworth <joeellingworth@gmail.com>
2020-04-30 09:27:37 +0100

    .

---
+++ spec/ValidationSpec.js
@@ -0,0 +1,6 @@
+describe("Validation", function() {
+
+  it('valid should be true until not', function(){
+    expect(this.valid).toEqual(true)
+  })
+});
---
+++ src/Validation.js
@@ -0,0 +1,3 @@
+function Validation(password) {
+  this.valid = true
+}
