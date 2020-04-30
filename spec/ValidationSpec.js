db9e8fa8cec8dc8bb31a9b5678379d2c8006c849
Joe Ellingworth <joeellingworth@gmail.com>
2020-04-30 12:51:37 +0100

    created Password class

--- spec/ValidationSpec.js
+++ spec/ValidationSpec.js
@@ -1,6 +1,13 @@
 describe("Validation", function() {
 
   it('valid should be true until not', function(){
-    expect(this.valid).toEqual(true)
+    var password = new Password("djI38D55")
+    console.log(password.password)
+    expect(password.validate()).toEqual(true)
   })
+
+  it('should return false if password is too short', function() {
+    expect(validate("123")).toEqual(false)
+  })
+
 });
--- src/Validation.js
+++ src/Validation.js
@@ -1,3 +1,16 @@
-function Validation(password) {
-  this.valid = true
+function Password(pw) {
+  this.password = pw
+  this.valid = true 
 }
+
+Password.prototype.validate = function() {
+  // checkLength(this.password)
+  return this.valid
+}
+  
+  function checkLength() {
+    valid = (this.password.length > 6) ? true : false;
+  }
+
+
+
